3293bcfdd68c79acd6798addcf98169899d94b12
Olga <olya_potapova@mail.ru>
2022-11-15 11:19:31 +0300

    The end

---
+++ Task1/Program.cs
@@ -0,0 +1,54 @@
+﻿bool CheckingTheSymbol(char symb)
+{
+    if (symb == '-' || symb == ',' || symb == '0' || symb == '1' || symb == '2' || symb == '3' || symb == '4' || symb == '5' || symb == '6' || symb == '7' || symb == '8' || symb == '9')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+Console.Write("Введите числа через запятую: ");
+string stringOfNum = Console.ReadLine();
+string numStr = String.Empty;
+int count = 0;
+
+for (int i = 0; i < stringOfNum.Length; i++)
+{
+    if (CheckingTheSymbol(stringOfNum[i]) == false)
+    {
+        Console.WriteLine("Вы ввели числа неправильно!");
+        return;
+    }
+    if (stringOfNum[i] == ',' && numStr == String.Empty)
+    {
+        Console.WriteLine("Вы ввели числа неправильно!");
+        return;
+    }
+    else if (stringOfNum[i] == ',' && numStr != String.Empty)
+    {
+        if (Convert.ToInt32(numStr) > 0)
+        {
+            count++;
+        }
+
+        numStr = String.Empty;
+    }
+    else
+    {
+        numStr = numStr + stringOfNum[i];
+    }
+
+}
+
+if (numStr != String.Empty)
+{
+    if (Convert.ToInt32(numStr) > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Число положительных чисел = {count}");
---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿double ReadDouble(string message)
+{
+    Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double b1 = ReadDouble("Введите b1: ");
+double k1 = ReadDouble("Введите k1: ");
+double b2 = ReadDouble("Введите b2: ");
+double k2 = ReadDouble("Введите k2: ");
+
+double x = (b2 * k1 - b1  * k1)/(k1 * (k1 - k2));
+double y = (b2 * k1 - k2 * b1)/(k1 - k2);
+
+Console.WriteLine($"({x},{y})");
